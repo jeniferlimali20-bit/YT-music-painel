8e18c4adfe8040f2530e688166a2136b5e337348
jeniferlimali20-bit <jeniferlimali20@gmail.com>
2026-03-05 20:50:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 self.addEventListener('install', (e) => {
   console.log('Service Worker instalado!');
+  // Força o Service Worker novo a assumir o controle imediatamente
+  self.skipWaiting(); 
+});
+
+self.addEventListener('activate', (e) => {
+  // Garante que o SW controle a página na hora, sem precisar recarregar
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
