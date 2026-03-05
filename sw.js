63796ba1656f67f70c655f99275ac0e2b41c65cb
jeniferlimali20-bit <jeniferlimali20@gmail.com>
2026-03-05 19:44:48 -0300

    Add install event listener to service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
-self.addEventListener('fetch', function(event) {
-  // Necessário para o navegador permitir a instalação
+self.addEventListener('install', (e) => {
+  console.log('Service Worker instalado!');
 });
 
+self.addEventListener('fetch', (e) => {
+  // Necessário para o PWA ser considerado "instalável"
+  e.respondWith(fetch(e.request));
+});
