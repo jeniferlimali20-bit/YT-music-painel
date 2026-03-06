1b31b4d8653ee10ee625457eeba6a7d67ddeefed
jeniferlimali20-bit <jeniferlimali20@gmail.com>
2026-03-05 22:58:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker instalado!');
-  // Força o Service Worker novo a assumir o controle imediatamente
-  self.skipWaiting(); 
+const CACHE_NAME = 'master-injector-v7';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (e) => {
-  // Garante que o SW controle a página na hora, sem precisar recarregar
-  return self.clients.claim();
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Necessário para o PWA ser considerado "instalável"
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+  self.clients.claim();
 });
