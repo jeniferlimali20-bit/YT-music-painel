f192a33703df78dbf63396a761bd12356fdd64c4
jeniferlimali20-bit <jeniferlimali20@gmail.com>
2026-03-05 23:56:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
 const CACHE_NAME = 'yt-musc-v8';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon512.png'
+  'index.html',
+  'manifest.json',
+  'icon512.png'
 ];
 
 self.addEventListener('install', event => {
+  // Força o SW a se tornar ativo imediatamente
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
@@ -23,9 +19,21 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
       }));
     })
   );
-  self.clients.claim();
+  // Garante que o SW controle a página imediatamente
+  return self.clients.claim();
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
